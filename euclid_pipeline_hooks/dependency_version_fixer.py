33128f585970d19a1106f11e09fc450c1cfee237
Hubert Degaudenzi <Hubert.Degaudenzi@gmail.com>
2023-11-07 17:02:39 +0100

    More fixes for flake8

--- euclid_pipeline_hooks/dependency_version_fixer.py
+++ euclid_pipeline_hooks/dependency_version_fixer.py
@@ -1,25 +1,21 @@
 #!/usr/bin/env python3
-
 # Copyright (C) 2012-2023, UNIGE and contributors (for the Euclid Science Ground Segment)
 # This file is part of Elements <https://gitlab.euclid-sgs.uk/ST-TOOLS/Elements>
 # SPDX-License-Identifier: LGPL-3.0-or-later
-
 """ Script to fix the version of a Elements-base pipeline project dependencies
     It is meant to be run in the pre-commit hook framework
 """
-
 from __future__ import annotations
 
-from typing import Sequence
 import argparse
-import sys
+from typing import Sequence
 
 
 def _fix_file(filename: str) -> bool:
     return True
 
 
-def main(argv: Sequence[str] | None=None) -> int:
+def main(argv: Sequence[str] | None = None) -> int:
 
     parser = argparse.ArgumentParser()
     parser.add_argument('filenames', nargs='*', help='Filenames to fix')
