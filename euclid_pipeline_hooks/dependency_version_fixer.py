fc097c71086fd4083ae91b971349d6ddab82c479
Hubert Degaudenzi <Hubert.Degaudenzi@gmail.com>
2023-11-08 16:22:45 +0100

    Initial implementation

--- euclid_pipeline_hooks/dependency_version_fixer.py
+++ euclid_pipeline_hooks/dependency_version_fixer.py
@@ -8,22 +8,139 @@
 from __future__ import annotations
 
 import argparse
+import re
 from typing import Sequence
 
+_TXT_PLAIN_VERSION_STYLE = r'(?:\d+)\.(?:\d+)(?:\.(?:\d+))?'
+NAME_AND_VERSION = re.compile(r'(\w+)\s+(%s)' % _TXT_PLAIN_VERSION_STYLE)
 
-def _fix_file(filename: str) -> bool:
-    return True
+
+def _normalize_entry(entry: str) -> str:
+    return ' '.join(entry.replace('\n', ' ').strip().split())
+
+
+def _filter_comments(input_text: str, comment: str = '#') -> str:
+    output_lines = []
+
+    for line in input_text.splitlines():
+        if not line.lstrip().startswith(comment):
+            output_lines.append(line)
+
+    return '\n'.join(output_lines)
+
+
+def _get_main_entry(content: str) -> str:
+    main_string = ''
+
+    main_string_match = re.compile(r'[^#\n]*elements_project\s*\((.+?)(?=USE|DESCRIPTION|\))', re.DOTALL)
+
+    m = main_string_match.search(content)
+
+    if m:
+        main_string = _normalize_entry(m.group(1))
+
+    return main_string
+
+
+def _get_name_and_version(content: str) -> tuple[str, str]:
+    name = ''
+    version = ''
+
+    main_entry = _get_main_entry(content)
+
+    m = NAME_AND_VERSION.match(main_entry)
+
+    if m:
+        name = m.group(1)
+        version = m.group(2)
+
+    return name, version
+
+
+def _get_dependency_entry(content: str) -> str:
+    use_string = ''
+
+    use_string_match = re.compile(r'[^#\n]*elements_project\s*\(.+USE((?s:.+?))(?=DESCRIPTION|\))', re.DOTALL)
+
+    m = use_string_match.search(content)
+
+    if m:
+        use_string = _normalize_entry(m.group(1))
+
+    return use_string
+
+
+def _get_dependencies(content: str) -> list[tuple[str, str]]:
+    dependencies = []
+
+    entry = _get_dependency_entry(content)
+
+    m = NAME_AND_VERSION.findall(entry)
+
+    if m:
+        dependencies = m
+
+    return dependencies
+
+
+def _get_projects(content: str) -> list[tuple[str, str]]:
+    projects: list[tuple[str, str]] = []
+
+    content = _filter_comments(content)
+
+    projects.append(_get_name_and_version(content))
+    projects += _get_dependencies(content)
+
+    return projects
+
+
+def _sub(content: str, project_name: str, new_version: str, project_version: str = '') -> str:
+
+    txt_search_project = fr'({project_name})([\n\s]+|/)({_TXT_PLAIN_VERSION_STYLE})'
+
+    if project_version:
+        txt_search_project = r'({})([\n\s]+|/)({})'.format(project_name, project_version.replace('.', r'\.'))
+
+    search_project = re.compile(txt_search_project)
+
+    new_content = search_project.sub(r'\g<1>\g<2>%s' % new_version, content)
+
+    return new_content
+
+
+def _fix_file(filename: str, projects: list[tuple[str, str]]) -> bool:
+    has_changed = False
+    with open(filename) as file_processed:
+        content = file_processed.read()
+
+    new_content = content
+    for project, version in projects:
+        new_content = _sub(new_content, project, version)
+
+    if content != new_content:
+        with open(filename, mode='w') as file_processed:
+            file_processed.write(new_content)
+        has_changed = True
+
+    return has_changed
 
 
 def main(argv: Sequence[str] | None = None) -> int:
+    return_code = 0
 
     parser = argparse.ArgumentParser()
     parser.add_argument('filenames', nargs='*', help='Filenames to fix')
     args = parser.parse_args(argv)
 
-    return_code = 0
+    # print(f'Current working directory {os.getcwd()}')
+
+    with open('CMakeLists.txt') as cmake_list:
+        content = cmake_list.read()
+
+    projects = _get_projects(content)
+
     for filename in args.filenames:
-        if _fix_file(filename):
+        if _fix_file(filename, projects):
             print(f'Fixing {filename}')
             return_code = 1
 
