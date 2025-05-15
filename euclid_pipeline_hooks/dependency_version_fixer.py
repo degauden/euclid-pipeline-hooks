9b340318680a87950d83ca690dc0a508251b9e85
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2025-05-15 15:11:38 +0200

    Fix from the pre-commit

--- euclid_pipeline_hooks/dependency_version_fixer.py
+++ euclid_pipeline_hooks/dependency_version_fixer.py
@@ -26,7 +26,7 @@ def _normalize_entry(entry: str) -> str:
     return ' '.join(entry.replace('\n', ' ').strip().split())
 
 
-def _filter_comments(input_text: str, comment: str='#') -> str:
+def _filter_comments(input_text: str, comment: str = '#') -> str:
     output_lines = []
 
     for line in input_text.splitlines():
@@ -101,7 +101,7 @@ def _get_projects(content: str) -> list[tuple[str, str]]:
     return projects
 
 
-def _sub(content: str, project_name: str, new_version: str, project_version: str='') -> str:
+def _sub(content: str, project_name: str, new_version: str, project_version: str = '') -> str:
 
     txt_search_project = fr'({project_name})([\n\s]+|/)({_TXT_PLAIN_VERSION_STYLE})'
 
@@ -126,7 +126,7 @@ def _has_dev_version(projects: list[tuple[str, str]]) -> bool:
             break
 
     if (has_dev):
-        print("One of the projects has a dev version")
+        print('One of the projects has a dev version')
 
     return has_dev
 
@@ -166,7 +166,7 @@ def _is_selected(entry_path: str, filters: list[str]) -> bool:
     return selected
 
 
-def main(argv: Sequence[str] | None=None) -> int:
+def main(argv: Sequence[str] | None = None) -> int:
     return_code = 0
 
     parser = argparse.ArgumentParser()
@@ -211,9 +211,10 @@ def main(argv: Sequence[str] | None=None) -> int:
 
     for entry in repo.commit().tree.traverse():
         entry_path = entry.path
-        if _is_selected(entry_path, all_filters) and _fix_file(entry_path, projects, args.with_cvmfs_branch, has_dev_version):
-            print(f'Fixing {entry_path}')
-            return_code = 1
+        if _is_selected(entry_path, all_filters):
+            if _fix_file(entry_path, projects, args.with_cvmfs_branch, has_dev_version):
+                print(f'Fixing {entry_path}')
+                return_code = 1
 
     return return_code
 
